e98ec7dc197186d609a6622695b1d22c21b67875
tshivanshu9 <tshivanshu9@gmail.com>
2025-07-02 22:56:22 +0530

    Dark mode v10

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
+import About from './components/About';
 import Navbar from './components/Navbar';
-import TextForm from './components/TextForm';
+// import TextForm from './components/TextForm';
 
 function App() {
   return (
@@ -8,7 +9,8 @@ function App() {
       {/* <Navbar title = "textutils" aboutText = "Aboutext"/> */}
       <Navbar/>
       <div className="container my-3">
-      <TextForm heading = "Enter your text to analyze"/>
+      {/* <TextForm heading = "Enter your text to analyze"/> */}
+      <About/>
       </div>
     </>
   );
