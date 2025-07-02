111e935dd95ce597e1101873a807eed7be35f773
tshivanshu9 <tshivanshu9@gmail.com>
2025-07-02 20:31:31 +0530

    Learnt useState hook

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
 
 function App() {
   return (
     <>
       {/* <Navbar title = "textutils" aboutText = "Aboutext"/> */}
       <Navbar/>
+      <div className="container my-3">
+      <TextForm heading = "Enter your text to analyze"/>
+      </div>
     </>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export default function Navbar({title,aboutText}) {
+export default function Navbar({title = 'TextUtils', aboutText = 'About'}) {
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
       <div className="container-fluid">
