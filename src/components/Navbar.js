491aadf9def47b0d82c35fd3bb76b43564b85682
tshivanshu9 <tshivanshu9@gmail.com>
2025-07-03 15:38:17 +0530

    Deployed on github pages

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
+// import { a } from 'react-router-dom';
 
 export default function Navbar({ title = 'TextUtils', aboutText = 'About', mode = 'light', toggleMode, color, toggleColor, toggleGreenColor }) {
     return (
         <nav className={`navbar navbar-expand-lg navbar-${mode} bg-${mode}`}>
             <div className="container-fluid">
-                <Link className="navbar-brand" to="/">{title}</Link>
+                <a className="navbar-brand" href="/">{title}</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+                            <a className="nav-link active" aria-current="page" href="#">Home</a>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" to="/about">{aboutText}</Link>
+                            <a className="nav-link" href="/about">{aboutText}</a>
                         </li>
                     </ul>
                     {/* <form className="d-flex" role="search">
